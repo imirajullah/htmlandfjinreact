bab7ad083ff8e30fc83254cc28e1de99ea407f17
Mirajullah <mirajullah95@gmail.com>
2025-07-16 11:33:23 +0500

    jtbu

--- reactgen/reactconcept/src/components/Contact/index.jsx
+++ reactgen/reactconcept/src/components/Contact/index.jsx
@@ -4,13 +4,40 @@ import profile from "../../img/profile.jpg"; // ✅ Using it below
 
 function Contact() {
   return (
-    <div className="contact-file">
-      <img src={profile} alt="Profile" className="profile" />
-      <h1>Miraj</h1>
-      <p>03025726323</p>
-      <p>mirajullah95@gmail.com</p>
-    </div>
+    <div className="contact-f">
+        <div className="contact-o">
+      <div className="contact-file">
+        <img src={profile} alt="Profile" className="profile" />
+        <h1>Miraj</h1>
+        <p>03025726323</p>
+        <p>mirajullah95@gmail.com</p>
+      </div>
+
+      <div className="contact-fil">
+        <img src={profile} alt="Profile" className="profile" />
+        <h1>Miraj</h1>
+        <p>03025726323</p>
+        <p>mirajullah95@gmail.com</p>
+      </div>
+      
+      </div>
+      <div className="contact-b">
+       <div className="contact-fil">
+        <img src={profile} alt="Profile" className="profile" />
+        <h1>Miraj</h1>
+        <p>03025726323</p>
+        <p>mirajullah95@gmail.com</p>
+      </div>
+       <div className="contact-fil">
+        <img src={profile} alt="Profile" className="profile" />
+        <h1>Miraj</h1>
+        <p>03025726323</p>
+        <p>mirajullah95@gmail.com</p>
+      </div>
+      </div>
+    </div> // ✅ This was fixed
   );
 }
 
 export default Contact;
+
