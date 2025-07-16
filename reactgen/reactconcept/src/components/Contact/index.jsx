8fb0bccc7ed8b69f51cd054cd04e58f8ee105107
Mirajullah <mirajullah95@gmail.com>
2025-07-16 10:31:15 +0500

    Contact

---
+++ reactgen/reactconcept/src/components/Contact/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css"
+import profile  from "../../img/profile.jpg"; // Adjust the path as needed
+
+function Contact() {
+        return(
+            <div className="contact-file">
+                <img src="{profile}" alt="profile" className="profile"/>
+                <Miraj/>
+                <p></p>"
+
+            </div>
+        )
+
+
+}
