43798946cf18ace234ff61431cfe5eac50bc0486
Mirajullah <mirajullah95@gmail.com>
2025-07-16 10:33:58 +0500

    contact

--- reactgen/reactconcept/src/components/Contact/index.jsx
+++ reactgen/reactconcept/src/components/Contact/index.jsx
@@ -6,11 +6,10 @@ function Contact() {
         return(
             <div className="contact-file">
                 <img src="{profile}" alt="profile" className="profile"/>
-                <Miraj/>
-                <p></p>"
-
+                <h1>Miraj</h1>
+                <p>03025726323</p>
+                <p>mirajullah95@gmail.com</p>
             </div>
         )
-
-
 }
+export default Contact;
