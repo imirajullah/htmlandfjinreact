1c022ae73bcc215de6ed458f0f417948c788828c
Mirajullah <mirajullah95@gmail.com>
2025-07-24 12:09:06 +0500

    timer

---
+++ memes/memes-maker/src/component/Timer/Time.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Timer() {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  let greeting="";
+  if (hour < 8) {
+    greeting = "Good morning";
+  } else if (hour < 14) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good evening";
+  }
+
+  return (
+    <div>
+      <h1>Greeting: {greeting}</h1>
+      <p>Current Time: {hour}:{minute < 10 ? `0${minute}` : minute}</p>
+    </div>
+  );
+}
+
+export default Timer;
